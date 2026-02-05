b4bdc2b1a68bfe102e81edf9a61afb4a58d77e51
HosseinEsteki <hosseinesteki77@gmail.com>
2026-02-05 22:22:09 +0330

    add organization model

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,6 +19,7 @@ class DatabaseSeeder extends Seeder
     {
         User::factory(10)->create();
         $this->call([
+            OrganizationSeeder::class,
             BlogSeeder::class,
             EcommerceSeeder::class,
         ]);
